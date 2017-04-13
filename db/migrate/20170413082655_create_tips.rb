e3fa038b4a3d3ecbee4ef6449a116cfc1e82e81c
bortch89 <bortch@gmail.com>
2017-04-13 10:53:56 +0200

    migrated migrations files to create new tables, and tested to see if everything works in Tux. All is fine.

--- db/migrate/20170413082655_create_tips.rb
+++ db/migrate/20170413082655_create_tips.rb
@@ -1,4 +1,11 @@
 class CreateTips < ActiveRecord::Migration[5.0]
   def change
+    create_table :tips do |t|
+      t.string :content
+      t.string :category
+      t.integer :votes
+      t.string :user_id
+      t.string :city_id
+    end
   end
 end
