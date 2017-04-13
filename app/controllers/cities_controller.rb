97aefd29120757568ca7bfa15439ea0de37dc9a6
bortch89 <bortch@gmail.com>
2017-04-13 16:37:43 +0200

    Added ability to edit cities, also made /cities list unique, in case someone edits an existing city to have the same name as another city. Not sure how to block this, that would be better.

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -1,7 +1,7 @@
 class CitiesController < ApplicationController
 
   get '/cities' do
-    @cities = City.all.sort_by {|city| city[:name] }
+    @cities = City.all.uniq.sort_by {|city| city[:name] }
     erb :'/cities/index'
   end
 
@@ -17,11 +17,26 @@ class CitiesController < ApplicationController
 
   post '/cities' do
     @new_city = City.new(name: params[:name], country: params[:country])
-    if !City.find_by_slug(@new_city.slug)
+    if !City.find_by_slug(@new_city.slug) && params[:name] != "" && params[:country] != ""
       @new_city.save
       redirect to "/cities/#{@new_city.slug}"
     else
       redirect to "/cities"
+    end
+  end
+
+  get '/cities/:slug/edit' do
+    @city = City.find_by_slug(params[:slug])
+    erb :'/cities/edit'
+  end
+
+  patch '/cities/:slug' do
+    @city = City.find_by_slug(params[:slug])
+    if params[:name] != "" && params[:country] != ""
+      @city.update(name: params[:name], country: params[:country])
+      redirect to "/cities/#{@city.slug}"
+    else
+      redirect to "/cities/#{@city.slug}/edit"
     end
   end
 end
