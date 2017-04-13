668dc78f18815c73cb74c89d704c93bb66684548
bortch89 <bortch@gmail.com>
2017-04-13 20:01:48 +0200

    created login page, seems to work so far

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,4 +29,22 @@ class UsersController < ApplicationController
     redirect "/"
   end
 
+  get '/login' do
+    if logged_in?
+      redirect "/"
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/"
+    else
+      redirect "/signup"
+    end
+  end
+
 end
