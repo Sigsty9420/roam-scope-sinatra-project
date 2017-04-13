3701a6abb9cb81168895ff694fb99d3f8f4396d8
bortch89 <bortch@gmail.com>
2017-04-13 19:40:48 +0200

    added quite a bit to the create_user view and user controller, also did some tests and realised I hadn't added the secure password macro to the user model, so added that too.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,31 @@
 class UsersController < ApplicationController
 
-  helpers do
-    def logged_in?
-      !!session[:user_id]
-    end
+  get '/login' do
+    erb :'users/login'
+  end
 
-    def current_user
-      User.find(session[:user_id])
+  get '/signup' do
+    if logged_in?
+      redirect "/"
+    else
+      erb :'/users/create_user'
     end
+  end
+
+  post '/signup' do
+    @user = User.create(username: params[:username], email: params[:email] , password: params[:password])
+
+    if @user.save && @user.username != "" && @user.email != ""
+      session[:user_id] = @user.id
+      redirect "/"
+    else
+      redirect "/signup"
+    end
+  end
+
+  get '/logout' do
+    session.clear
+    redirect "/"
   end
 
 end
