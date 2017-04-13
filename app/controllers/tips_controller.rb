a0053b3f54bc71a34414a6f58bce40f05486eb60
bortch89 <bortch@gmail.com>
2017-04-13 19:10:40 +0200

    started working on tips controller, added path to create a new tip. It is not complete yet as users need to be able to login first, so I will create the signup and login page before continuing

--- app/controllers/tips_controller.rb
+++ app/controllers/tips_controller.rb
@@ -1,3 +1,15 @@
 class TipsController < ApplicationController
 
+  get '/tips/new' do
+    if logged_in?
+      erb :"/tips/new"
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/tips' do
+    
+  end
+
 end
