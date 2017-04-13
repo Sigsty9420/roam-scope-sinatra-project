da8f8bc7f1e18cdd69f211876455418e5a9f17a6
bortch89 <bortch@gmail.com>
2017-04-13 16:10:36 +0200

    added action to add new cities, and only allow users to add unique cities

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -5,9 +5,23 @@ class CitiesController < ApplicationController
     erb :'/cities/index'
   end
 
+  get '/cities/new' do
+    erb :"/cities/new"
+  end
+
   get '/cities/:slug' do
     @city = City.find_by_slug(params[:slug])
     @tips = @city.tips
     erb :'/cities/show'
+  end
+
+  post '/cities' do
+    @new_city = City.new(name: params[:name], country: params[:country])
+    if !City.find_by_slug(@new_city.slug)
+      @new_city.save
+      redirect to "/cities/#{@new_city.slug}"
+    else
+      redirect to "/cities"
+    end
   end
 end
