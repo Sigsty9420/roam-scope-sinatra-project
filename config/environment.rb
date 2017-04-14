9aca988cb4dc54d3277f3a412cc0c48670d9422b
bortch89 <bortch@gmail.com>
2017-04-14 19:47:41 +0200

    fixed postgres db file url

--- config/environment.rb
+++ config/environment.rb
@@ -11,7 +11,7 @@ end
 
 configure :production do
   db =  URI.parse(ENV['DATABASE_URL'] || 'postgres_database_url')
-  db =  'postgres://still-beyond-16384.herokuapp.com/'
+  db =  'postgres://yryiiwfaozdlsr:79c1d4b8dd0b1b0c8118d4bf1bddd8a8e06452d5a836c5faa4916d82069bdb5c@ec2-54-225-182-108.compute-1.amazonaws.com:5432/da6hn1dtao4plk'
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
